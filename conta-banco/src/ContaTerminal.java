328264f37f8a634d272fae2cab651760fb957b4c
Anderson Rodrigo <andersonrodrigoam@gmail.com>
2024-06-14 11:34:28 -0300

    .

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -28,6 +28,7 @@ public class ContaTerminal {
         System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é "
                 + agencia + ", conta " + numeroConta +
                 " e seu saldo " + saldo + " já está disponível para saque.");
+                
         // Fecha o Scanner
         scan.close();
 
